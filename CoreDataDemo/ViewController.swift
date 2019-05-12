92096079baddd95745ad57398f4ae82119241d6e
kondya <konds9975@gmail.com>
2019-05-12 14:09:02 +0530

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Kondya on 12/05/19.
+//  Copyright © 2019 Kondya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
